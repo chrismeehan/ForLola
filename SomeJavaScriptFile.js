9740e37fe8f5027304ca5f225beea0703a377a7f
Meehan <christopher_meehan@christopher-meehans-MacBook-Pro.local>
2018-01-23 16:50:45 -0800

    Chris added an HTML file, CSS file, and Javascript file

---
+++ SomeJavaScriptFile.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+	// Inside Javascript.      window is the what javascript calls the html its talking to.
+	// onload means "once the entire webpage is done loading, and is ready to appear onscreen"
+	
+	// function just means "some task".  Like doing the dishes is a function.
+	
+	
+	
+	// So, once an html page loads onscreen, this area gets called to do whatever it wants to do.   Play videos, or totally manipulate the HTML to plug in adds or crazy stuff.
+	
+	// HTML or CSS by itself can do "anything" the way javascript can.
+	
+	
+	// The only thing were doing right now is displaying a message, when the page loads.
+	
+	alert("yo");
+	
+	
+}
